fe1736d2dfdc5d7b487b64e46a56db1d826de018
Tyler Chang <chang.ty97@gmail.com>
2022-12-19 22:54:19 -0800

    Updated files

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-from govee_api2 import api, device
 from getCredentials import getCredentials
-import time
-import colour
+from govee_api_ble import GoveeDevice
 
-def govee_connection(email, password, client_id=None):
-     # Create Govee client and configure event handlers
-    govee_cli = api.Govee(email, password, client_id)
-    # BEWARE: This will create a new Govee Client ID with every login. It is recommended to provide an existing client ID
-    # within the `Govee` contructor. You can fetch your generated client ID via `govee.client_id` after your successful login
 
-    # Login to Govee
-    govee_cli.login()
+def govee_connection():
+    # Initialize the device
+    my_device = GoveeDevice('34:20:03:49:72:3D')
 
-    # Print out the used client ID
-    print('Current client ID is: {}'.format(govee_cli.client_id))
-
-    # Fetch known devices from server
-    govee_cli.update_device_list()
-
-    print('Preparing for action :-)')
-    # Don't do this in real life. Use the callbacks the client provides to you!
-    time.sleep(10)
-
+    my_device.setPower(True) # Turns device on
+    
 if __name__ == "__main__":
-    email = getCredentials().getEmail()
-    password = getCredentials().getPassword()
-    client_id = getCredentials().getClientID()
-    print(client_id)
+    govee_connection()
