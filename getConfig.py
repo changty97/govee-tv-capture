9141a48c7731205e0ea576959a51443f4ae7bf0c
Tyler Chang <chang.ty97@gmail.com>
2022-12-20 23:33:50 -0800

    Updated pylint file and fixed errors

--- getConfig.py
+++ getConfig.py
@@ -1,17 +1,38 @@
+"""Module get MAC Address from config file."""
 import configparser
 import logging
 
-class getConfig():
 
-    def __init__(self): 
+class getConfig:
+    """
+    A class to get a MAC Address.
+
+    ...
+
+    Methods
+    -------
+    setMAC():
+        Sets a MAC Address.
+    getMAC():
+        Returns MAC Address from the config file.
+    """
+
+    def __init__(self):
+        """Constructs all the necessary attributes for the getConfig object."""
         self.mac = None
         self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
-        
-        if self.config['ADDRESS']['MAC'] != "":
-            self.mac = self.config['ADDRESS']['MAC']
+        self.config.read("config.ini")
+        if self.config["ADDRESS"]["MAC"] != "":
+            self.setMAC(self.config["ADDRESS"]["MAC"])
         else:
-            logging.exception("MAC is blank in config.ini file please input a MAC Address.")
-        
+            logging.exception(
+                "MAC is blank in config.ini file please input a MAC Address."
+            )
+
+    def setMAC(self, mac):
+        """Sets the MAC Address"""
+        self.mac = mac
+
     def getMAC(self):
+        """Returns the MAC Address"""
         return self.mac
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from getConfig import getConfig
+"""Module connects to any Govee Device."""
 from govee_api_ble import GoveeDevice
+from .getConfig import getConfig
 
 
-def govee_connection():
+def goveeConnection():
+    """Creates a connection to a Govee device via Bluetooth"""
     # Initialize the device
     mac_address = getConfig().getMAC()
     my_device = GoveeDevice(mac_address)
 
-    my_device.setPower(True) # Turns device on
-    
+    my_device.setPower(True)  # Turns device on
+
+
 if __name__ == "__main__":
-    govee_connection()
+    goveeConnection()
